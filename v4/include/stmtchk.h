8d62bd10f8c4767201d7f9c205cecd7745da2f4f
EricXiao <chuganghong@qq.com>
2022-04-26 15:17:01 +0800

    v4--增加语句解析文件

---
+++ v4/include/stmtchk.h
@@ -0,0 +1,10 @@
+#ifndef STMTCHK_H
+#define STMTCHK_H
+
+#include "ast.h"
+#include "lex.h"
+#include "stmt.h"
+#include "type2.h"
+#include "expr.h"
+
+#endif
---
+++ v4/stmtchk.c
@@ -0,0 +1,7 @@
+#include "symbol.h"
+#include "ast.h"
+#include "stmt.h"
+#include "decl.h"
+#include "expr.h"
+#include "stmtchk.h"
+
